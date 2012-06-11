fb0eaf7f17cad33f06d3436c4a3fbe85a011e130
Lukas Martini <hello@lutoma.org>
2012-06-11 09:05:46 +0200

    Correctly pass MEDIA_URL to templates

--- opensoup/settings.py
+++ opensoup/settings.py
@@ -93,6 +93,11 @@ TEMPLATE_LOADERS = (
 #	 'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+	'django.contrib.auth.context_processors.auth',
+	'django.core.context_processors.media',
+)
+
 MIDDLEWARE_CLASSES = (
 	'django.middleware.common.CommonMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
--- tumblelog/views.py
+++ tumblelog/views.py
@@ -2,12 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404, render_to_response
 from django.contrib.auth.decorators import login_required
 from tumblelog.models import Post, ImagePost
+from django.template.context import RequestContext
 
 def everyone(request):
 	"Shows everyone's posts"
 
 	posts = ImagePost.objects.all().order_by('-id')[:20]
-	return render_to_response('tumblelog/everyone.html', {'posts': posts})
+	return render_to_response('tumblelog/everyone.html', context_instance=RequestContext(request, {'posts': posts}))
 
 @login_required
 def friends(requests):
