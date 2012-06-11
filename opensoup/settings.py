5216f6b1d4c84dce7baf5113fa8a4924c37b7b99
Lukas Martini <hello@lutoma.org>
2012-06-11 08:56:59 +0200

    Properly serve assets and staticfiles in development

--- opensoup/settings.py
+++ opensoup/settings.py
@@ -50,18 +50,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/tmp/'
+MEDIA_ROOT = '/tmp/opensoup-asset/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/asset/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/tmp/opensoup-static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- opensoup/urls.py
+++ opensoup/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
-urlpatterns = patterns('',
+if settings.DEBUG:
+	urlpatterns = patterns('django.views.static',
+		url(r'^static/(?P<path>.*)$',  'serve', {'document_root': '/tmp/opensoup-static/', 'show_indexes': True }),
+		url(r'^asset/(?P<path>.*)$',  'serve', {'document_root': '/tmp/opensoup-asset/', 'show_indexes': True }),
+	)
+else:
+	urlpatterns = patterns('')
+
+urlpatterns += patterns('',
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('tumblelog.urls')),
