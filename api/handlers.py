df8ba02feef76901752a2c577d63eb173c4dce90
Lukas Martini <hello@lutoma.org>
2012-06-15 12:12:02 +0200

    Adding asset API handler

--- api/handlers.py
+++ api/handlers.py
@@ -1,12 +1,24 @@
 from piston.handler import BaseHandler
-from tumblelog.models import Post
+from tumblelog.models import Post, Asset
 
 class PostHandler(BaseHandler):
 	allowed_methods = ('GET',)
+	exclude = ('_state')
 	model = Post
 
 	def read(self, request, post_id = None):
 		if post_id:
 			return Post.objects.filter(id = post_id)
-		
+
 		return Post.objects.all()
+
+class AssetHandler(BaseHandler):
+	allowed_methods = ('GET',)
+	exclude = ('_state')
+	model = Asset
+
+	def read(self, request, asset_id = None):
+		if asset_id:
+			return Asset.objects.filter(id = asset_id)
+		
+		return Asset.objects.all()
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from piston.resource import Resource
-from api.handlers import PostHandler
+from api.handlers import PostHandler, AssetHandler
 
 post_handler = Resource(PostHandler)
+asset_handler = Resource(AssetHandler)
 
 urlpatterns = patterns('',
 	url(r'^posts/(?P<post_id>[^/]+)/', post_handler),
 	url(r'^posts/$', post_handler),
+
+	url(r'^assets/(?P<asset_id>[^/]+)/', asset_handler),
+	url(r'^assets/$', asset_handler),
 )
